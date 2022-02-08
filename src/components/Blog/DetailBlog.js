00177455335670a4cf664fe27a18ab3b85e0e819
DanhBui <bxdanh1998@gmail.com>
2022-02-08 22:24:12 +0700

    detail blog

--- src/components/Blog/DetailBlog.js
+++ src/components/Blog/DetailBlog.js
@@ -1,8 +1,13 @@
 import { useParams, useHistory } from "react-router-dom";
+import useFetch from "../../customize/fetch";
 
 const DetailBlog = ()=>{
     let {id} = useParams()
     let history = useHistory()
+
+    const {data:dataBlogDetail, isLoading, isError} = useFetch
+  (`https://jsonplaceholder.typicode.com/posts/${id}`, false)
+
     const handleBackBlogData =()=>{
         history.push('/blog')
     }
@@ -13,7 +18,25 @@ const DetailBlog = ()=>{
                     &lt;-- Back
                 </span>
             </div> 
-            <h1>Detail blog with id= {id}</h1>
+            <div className="blog-detail">
+                {isLoading === false && isError===false && dataBlogDetail  && 
+                  <>
+                    <div className="title">
+                        {dataBlogDetail.title}
+
+                    </div>
+                    <div className="content">
+                        {dataBlogDetail.body}
+
+                    </div>             
+                  </>
+
+                }
+                {isLoading === true &&
+                  <div>Loading data...</div>
+
+                }
+            </div>
         </div>
     )
 }
