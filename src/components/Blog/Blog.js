b6c02c141ee15cabc6dddbbf25949abd08db0d25
DanhBui <bxdanh1998@gmail.com>
2022-02-12 20:47:01 +0700

    modal add new blog

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import useFetch from '../../customize/fetch';
-import {Link, useHistory} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import {useState} from 'react'
 
+
+import useFetch from '../../customize/fetch';
+import AddNewBlog from './AddNewBlog';
 import './Blog.scss';
 
 const Blog = () => {
 
-  let history = useHistory()
+  const [show, setShow] = useState(false);
+
 
   const {data:dataBlog, isLoading, isError} = useFetch
   (`https://jsonplaceholder.typicode.com/posts`, false)
@@ -18,31 +22,49 @@ const Blog = () => {
 
   }
   const handleAddNewBlog = ()=>{
-    history.push('/add-new-blog')
-
+       setShow(true)
+  }
+  const handleCloseModal = () => {
+    setShow(false)
   }
   return (
     <>
-      <div >
-        <button className="btn-add-new-blog" onClick ={handleAddNewBlog}>
-            + Add new Blog
-        </button>
-      </div>
-      <div className="blog-container">
-        { isLoading === false && isError === false &&newDataBlog && newDataBlog.length>0 && 
-        newDataBlog.map(item =>(
-            <div key={item.id} className="single-blog">
-                 <p className="title">{item.title}</p>
-                 <p className="body">{item.body}</p>
-                   <Link to={`/blog/${item.id}`} exact="true">
-                      View details
-                   </Link>
+        <div >
+          <button className="btn-add-new-blog" onClick ={handleAddNewBlog}>
+              + Add new Blog
+          </button>
+        </div>
+        {
+          show && 
+          <div className="modal-add-new-blog" onClick={handleCloseModal}>
+            <div className="modal-container" onClick = {(e)=>e.stopPropagation()}>
+              <div className="modal-header">
+
+              </div>
+              <div className="modal-body">
+                <AddNewBlog/>
+
+              </div>
+              <button class="btn-modal-close" onClick={handleCloseModal}>Close</button>
+
             </div>
-        ))}
-        { isLoading === true && 
-          <div>Loading data...</div>
+          </div>
         }
-      </div>
+        <div className="blog-container">
+          { isLoading === false && isError === false &&newDataBlog && newDataBlog.length>0 && 
+          newDataBlog.map(item =>(
+              <div key={item.id} className="single-blog">
+                  <p className="title">{item.title}</p>
+                  <p className="body">{item.body}</p>
+                    <Link to={`/blog/${item.id}`} exact="true">
+                        View details
+                    </Link>
+              </div>
+          ))}
+          { isLoading === true && 
+            <div>Loading data...</div>
+          }
+        </div>
     </>
     )
 };
